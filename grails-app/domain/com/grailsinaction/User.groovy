f51f2e8838ec7a577058dedd4cc2210617f25f49
thomi137 <thomas@prosser.ch>
2016-07-14 22:00:27 +0200

    203 of 1105

--- grails-app/domain/com/grailsinaction/User.groovy
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -12,7 +12,6 @@ class User {
     static constraints = {
         loginId size: 3..20, unique: true, nullable: false
         password size: 6..8, nullable: false, validator: {passwd, user -> passwd != user.loginId}
-        homepage url: true, nullable: true
         profile nullable: true
     }
 }
---
+++ src/integration-test/groovy/com/grailsinaction/PostIntegrationSpec.groovy
@@ -0,0 +1,46 @@
+package com.grailsinaction
+
+
+import grails.test.mixin.integration.Integration
+import grails.transaction.*
+import spock.lang.*
+
+@Integration
+@Rollback
+class PostIntegrationSpec extends Specification {
+
+   def "Adding posts to user links post to user"(){
+
+       given: "A brand new user"
+       def user = new User(loginId: 'joe', password: 'secret')
+       user.save(failOnError: true)
+
+       when: "Several posts are added to the user"
+       user.addToPosts(new Post(content: "First post... W00t!"))
+       user.addToPosts(new Post(content: "Second post..."))
+       user.addToPosts(new Post(content: "Third post"))
+
+       then: "The user has a list of posts"
+       3 == User.get(user.id).posts.size()
+
+   }
+
+    def "Ensure posts linked to a user can be retrieved"(){
+
+        given: "A user with several posts"
+        def user = new User(loginId: 'joe', password: 'secret')
+        user.addToPosts(new Post(content: "First"))
+        user.addToPosts(new Post(content: "Second"))
+        user.addToPosts(new Post(content: "Third"))
+        user.save(failOnError: true)
+
+        when: "The user is retrieved by their id"
+        def foundUser = User.get(user.id)
+        def sortedPostContent = foundUser.posts.collect { it.content}.sort()
+
+        then: "The posts appear on the retrieved user"
+        sortedPostContent == ['First','Second', 'Third']
+
+
+    }
+}
